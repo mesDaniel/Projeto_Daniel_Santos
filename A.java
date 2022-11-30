a238291a643fc37ca0dedf77e7ba1eef752d1ebd
DanielSantos <danmes12345@gmail.com>
2022-11-30 14:32:31 -0300

    Primeiro passo

---
+++ A.java
@@ -0,0 +1,30 @@
+public class A{
+	private static int A1;
+	private static int A2;
+	
+	
+	public int get_A1(){
+		return A1;
+	}
+	
+	public int get_A2(){
+		return A2;
+	}
+	
+	public void set_A1(int a1){
+		A1 = a1;
+	}
+	
+	public void set_A2(int a2){
+		A2 = a2;
+	}
+	
+	public void MA1(){
+		System.out.println("MA1");
+	}
+	
+	public void MA2(){
+		System.out.println("MA2");
+	}
+	
+}
---
+++ B.java
@@ -0,0 +1,30 @@
+public class B{
+	private static int B1;
+	private static int B2;
+	
+	
+	public int get_B1(){
+		return B1;
+	}
+	
+	public int get_B2(){
+		return B2;
+	}
+	
+	public void set_B1(int b1){
+		B1 = b1;
+	}
+	
+	public void set_B2(int b2){
+		B2 = b2;
+	}
+	
+	public void MA1(){
+		System.out.println("MA1");
+	}
+	
+	public void MA2(){
+		System.out.println("MA2");
+	}
+	
+}
